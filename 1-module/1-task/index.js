479ee462bff14936456cfb15366ae56998e81497
akarpenko_jr <andy.karpenko737@gmail.com>
2019-02-23 14:30:11 +0300

    Added function pow & function for check number

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,6 +4,21 @@
  * @param {number} n index
  * @returns {number}
  */
-function pow (m, n) {
 
+//
+// Проверка вводимых данных на предмет соответствия типу "Число"
+//
+function isNumeric(x) {
+    return !isNaN(parseFloat(x)) && isFinite(x);
+}
+
+//
+// Возведение числа m в степень n
+//
+function pow (m, n) {
+    if (n != 1) { // пока n != 1, сводить вычисление pow(x,n) к pow(x,n-1)
+        return m * pow(m, n - 1);
+    } else {
+        return m;
+    }
 }
