abb8bfde537d340bc57b1acafdcbfc1f3a94ed75
akarpenko_jr <andy.karpenko737@gmail.com>
2019-02-24 17:06:29 +0300

    добавил функцию получения минимального и максимального значений

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -6,10 +6,23 @@
  * @returns {{min:number, max:number}}  объект
  */
 
-
+function cleanString (str) {
+    var newString = str.split(" ");
+    return newString;
+}
 
 function getMinMax(str) {
-    let string1 = str.split(" ");
-    return string1;
+    let minNumber = str[0];
+    let maxNumber = str[0];
+    for (let key in str) {
+        if (parseFloat(str[key]) > maxNumber) {
+            maxNumber = str[key];
+        }
+        if (parseFloat(str[key]) < minNumber) {
+            minNumber = str[key];
+        }
+    }
+    let minmaxArr = [minNumber, maxNumber];
+    return minmaxArr;
 }
 
