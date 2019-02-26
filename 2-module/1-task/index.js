74a47742d55ee2acaf39ea9349fb66b3a3794fb8
akarpenko_jr <andy.karpenko737@gmail.com>
2019-02-26 10:58:03 +0300

    добавил фукцию клонирования объекта через перебор свойст...далее доработка для обработки многослойного объеккта

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,5 +3,26 @@
  * @param {Object} obj - клонируем объект
  * @returns {Object}
  */
+/*
+Объявляем тестовую переменную
+ */
+let obj = {
+    test: 1,
+    test22: 2
+};
+
+/*
+clone
+функция клонирования объекта через перебор свойств
+ */
 function clone (obj) {
+    let target = {};
+    for (let prop in obj) {
+        if (obj.hasOwnProperty(prop)) {
+            target[prop] = obj[prop];
+        }
+    }
+    return target;
 }
+
+
