d4a6daf7e505222e310233b0a1a66cefc0563281
akarpenko_jr <andy.karpenko737@gmail.com>
2019-03-10 23:48:23 +0300

    2-2-решена

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -4,5 +4,53 @@
  * @param {*} value - значение, которе ищем
  * @returns {Object}
  */
-function find (obj, value) {
+
+let newObj = {
+    options: {
+        color: "red"
+    }
+};
+
+
+function find(obj, value){
+    const arrayOfPath = [];
+    let findPath;
+    const answerArray = [];
+    // перебор
+    for (let prop in obj) {
+        // если объект, то запускаем рекурсию
+
+        if (typeof obj[prop] === "object") {
+            if ( find(obj[prop], value) !== null) {
+                findPath = prop + '.' + find(obj[prop], value);
+                arrayOfPath.push(findPath);
+            }
+
+            // запускаем рекурсию, сохраняем результат в переменную
+
+        } else {
+            // проверяем взождение, сохраняем результат в переменную
+            if (obj[prop] === value) {
+                arrayOfPath.push(prop);
+                return arrayOfPath;
+            }
+
+
+        }
+    }
+
+
+
+    // отдаем результат, если неичего не нашли, то возвращем null
+    if (arrayOfPath.length !== 0) {
+        if (arrayOfPath.length === 1) {
+            return arrayOfPath.join();
+        } else {
+            return arrayOfPath;
+        }
+        // проверяем на количество найденных результатов и возвращаем строку или массив
+
+    } else {
+        return null;
+    }
 }
