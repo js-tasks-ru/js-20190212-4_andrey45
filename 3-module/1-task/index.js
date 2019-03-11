46a080d108ecd0848dab15b8887f823d6ac49552
akarpenko_jr <andy.karpenko737@gmail.com>
2019-03-11 19:39:36 +0300

    удалил проверочный массив

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,16 +5,8 @@
  * @returns {string}
  */
 "use strict";
-let data = [
-    {
-        "balance": "$1,825.65",
-        "picture": "http://placehold.it/32x32",
-        "age": 21,
-        "name": "Golden Branch",
-        "gender": "male",
-        "greeting": "Hello, Golden Branch! You have 7 unread messages.",
-        "favoriteFruit": "banana"
-    } ];
+
+
 function showSalary(data, age) {
     let userListArray = [];
     let userList = "";
