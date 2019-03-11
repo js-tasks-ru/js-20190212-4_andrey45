30745b75522042cab958966117d9a8d7af49e29f
akarpenko_jr <andy.karpenko737@gmail.com>
2019-03-11 18:59:39 +0300

    3-1-solved

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -4,7 +4,28 @@
  * @param {number} age - максимальный возраст
  * @returns {string}
  */
+
+let data = [
+    {
+        "balance": "$1,825.65",
+        "picture": "http://placehold.it/32x32",
+        "age": 21,
+        "name": "Golden Branch",
+        "gender": "male",
+        "greeting": "Hello, Golden Branch! You have 7 unread messages.",
+        "favoriteFruit": "banana"
+    } ];
 function showSalary(data, age) {
+    let userListArray = [];
+    userList = "";
+    let userNameSalary = '';
+    for (let i=0; i<=data.length-1; i++) {
+        if (data[i].age<=age){
+            userNameSalary = data[i].name + ", " + data[i].balance;
+            userListArray.push(userNameSalary);
+        }
+    }
+    userList = userListArray.join('\n');
+
+    return userList;
 }
-
-
