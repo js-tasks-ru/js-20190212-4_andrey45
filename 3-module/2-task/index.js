9c20a755a2384ff410eaaffcf3545015f383b2ee
akarpenko_jr <andy.karpenko737@gmail.com>
2019-03-14 22:13:00 +0300

    Добавил итератор по числам...выводит числа от указанного числа до конечного в нужном формате

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -6,4 +6,66 @@ let calendar = {
 calendar[Symbol.iterator] = function () {
 };
 
+let calendar = {
+    from: new Date('2018-01-01T15:09:10Z'),
+    to: new Date('2018-02-01T10:09:10Z')
+};
 
+calendar[Symbol.iterator] = function () {
+    let current = this.from;
+    let last = this.to;
+    let thisDate = "";
+    return {
+        next() {
+            if (current <= last) {
+                current.setDate(current.getDate() + 1);
+                if ((current.getDate() <= 9) && (current.getDate() <= 9)){
+                    thisDate = "0" + current.getDate();
+                } else if (current.getDate() >= 10) {
+                    thisDate = String(current.getDate());
+                }
+
+                return {
+                    done: false,
+                    value: thisDate
+                };
+            } else {
+                return {
+                    done: true
+                };
+            }
+        }
+    }
+};
+
+for (let day of calendar) {
+    console.log(day);
+}
+
+
+
+
+
+
+
+/**********************************************************************************************/
+// function buildFilters() {
+//     let string = `status:sample_received,status:on_hold,status:sent_to_another,pidor:sent_to_another`;
+//     let res = string.split(',');
+//     console.log(res);
+//     let newString = '';
+//     for (let i=1; i<=res.length-1; i++) {
+//         if(res[i].charAt(0)==="s" && (res[i].charAt(1)==="t" && (res[i].charAt(2)==="a" && (res[i].charAt(3)==="t" && (res[i].charAt(5)==="u" (res[i].charAt(6)==="s") {
+//             res[i] = "OR" + res[i];
+//         }
+//         res[i] = "AND" + res[i];
+//     }
+//     newString = res.join();
+//
+//     console.log(newString);
+//     return newString;
+// }
+//
+//
+// buildFilters();
+/**********************************************************************************************/
