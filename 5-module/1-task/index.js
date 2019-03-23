b1a33fb74884c05bfa42ec0cc3977ad5129b3c87
akarpenko_jr <andy.karpenko737@gmail.com>
2019-03-23 15:02:54 +0300

    добавил файл для тестирования функций test.html

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -5,4 +5,7 @@
  * @param {Element} table
  */
 function highlight(table) {
+    let teachersTable = document.getElementsByClassName("js-teachers");
+    let td = teachersTable.getElementsByTagName("td");
+    alert(td);
 }
