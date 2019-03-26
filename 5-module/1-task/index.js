7c118679de764688c29aed2a7c641146857dbfb2
akarpenko_jr <andy.karpenko737@gmail.com>
2019-03-26 11:38:34 +0300

    Добавил определния номера столбца для унификации поиска данных по таблице

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -5,7 +5,22 @@
  * @param {Element} table
  */
 function highlight(table) {
-    let teachersTable = document.getElementsByClassName("js-teachers");
-    let td = teachersTable.getElementsByTagName("td");
-    alert(td);
+    let headsOfTable = document.querySelector("thead tr").children;
+    let nameKey = 0;
+    let ageKey = 0;
+    let genderKey = 0;
+    let statusKey = 0;
+    for (let i = 0; i<headsOfTable.length; i++) {
+        if (headsOfTable[i].innerHTML === "Name") {
+            nameKey = i;
+        } else if (headsOfTable[i].innerHTML === "Age") {
+            ageKey = i;
+        } else if (headsOfTable[i].innerHTML === "Gender") {
+            genderKey = i;
+        } else if (headsOfTable[i].innerHTML === "Status") {
+            statusKey = i;
+        }
+
+    }
+    console.log(statusKey);
 }
