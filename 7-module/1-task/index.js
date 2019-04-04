70f8815c704bc082941eccaf9a8e44a2dd3a682e
akarpenko_jr <andy.karpenko737@gmail.com>
2019-04-04 19:26:43 +0300

    Doesn`t work

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -7,6 +7,21 @@
  * @param {Array} log - массив для записи логов
  * @return {Function}
  */
-function makeLogging(fn, log) {
-
+function work(a, b) {
+    return a + b;
 }
+
+function makeLogging(fn, log) {
+    function fn() {
+        for (let i=0; i<arguments.length; i++) {
+            return log.push(arguments[i]);
+        }
+    }
+}
+
+let log = [];
+
+let works = makeLogging(work, log);
+works(1, 2); // -> 3
+
+alert(log[0][0] === 1, log[0][1] === 2);
